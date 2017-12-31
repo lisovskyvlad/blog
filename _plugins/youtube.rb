1d381f3c8c3948acea3af0930effdedd9211ec94
Lisovsky Vlad <lisovskyvlad@gmail.com>
2017-12-31 23:54:09 +0100

    Add first version of post about 2017 and youtube plugin

---
+++ _plugins/youtube.rb
@@ -0,0 +1,20 @@
+class YouTube < Liquid::Tag
+  SYNTAX = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+
+  def initialize(_tag_name, markup, tokens)
+    super
+
+    match_data = SYNTAX.match(markup)
+    raise "No YouTube ID provided in the 'youtube' tag" unless match_data
+
+    @id = match_data[0]
+  end
+
+  def render(_context)
+    %(<iframe width="600" height="600"
+              src="http://www.youtube.com/embed/#{@id}?color=white&theme=light">
+      </iframe>)
+  end
+
+  Liquid::Template.register_tag 'youtube', self
+end
